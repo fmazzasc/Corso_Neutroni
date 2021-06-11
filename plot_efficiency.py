686f3880f8d77f5b52fbe1286bd2e40b47294ba5
fmazzasc <fmazzasc@cern.ch>
2021-06-11 10:50:27 +0200

    Fix efficiency plots

--- plot_efficiency.py
+++ plot_efficiency.py
@@ -3,6 +3,7 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 
+thickness = 1e-1
 
 files = os.listdir("result_arrays/")
 h=10 #cm
@@ -20,7 +21,7 @@ for file in files:
     thick = float(res[0])
     d = float(res[1])
     E = float(res[2])
-    if thick == 1e-1:
+    if thick==thickness:
         if E==1:
             print("E: ", E, "d: ", d, "t: ", thick)
             eff_arr_1.append(len(arr)/(d*np.pi**2))
@@ -61,12 +62,9 @@ eff_arr_5 = eff_arr_5[index_5]
 plt.plot(d_arr_1, eff_arr_1, ".-", label = "Neutron Energy = 1 MeV")
 plt.plot(d_arr_2, eff_arr_2,  ".-", label = "Neutron Energy = 2 MeV")
 plt.plot(d_arr_5, eff_arr_5,  ".-", label = "Neutron Energy = 5 MeV")
-title_string = f"Thickness: {thick} cm"
+title_string = f"Thickness: {thickness} cm"
 plt.legend(title=r'$\bf{{{}}}$'.format(title_string.replace(' ', r'\;')), fancybox=True)
 plt.xlabel("h/d (cm)", fontsize=13)
 plt.ylabel(r"# Protons / Collimator Area (cm$^{-2}$)", fontsize=13)
 
-plt.savefig(f"result_plots/efficiency_t_{thick}.png", bbox_inches="tight")
-# fig = plt.figure()
-# ax = fig.add_subplot()
-# ax.plot()
+plt.savefig(f"result_plots/efficiency_t_{thickness}.png", bbox_inches="tight")
